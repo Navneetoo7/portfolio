ded31175c925943e7e6021c051b12ffbc633a334
navneet <navneetjustin@gmail.com>
2025-03-29 15:51:21 +0400

    update resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "Accomplished Full Stack Developer with 5+ years of experience designing and implementing robust web and mobile applications across OTT, Oil Gas, Banking, and E-commerce domains. Proficient in cross-platform development using React Native and Angular, with expertise in TypeScript, Node.js, and Nest.js. Skilled in developing scalable microservices, integrating complex architectures, and delivering innovative solutions that drive operational efficiency",
   github: "https://github.com/Navneetoo7",
   resume:
-    "https://drive.google.com/file/d/10yfgGa6Ea2JlQ1OnRxfe-kZrXRxmgvvM/view?usp=sharing",
+    "https://drive.google.com/file/d/1qw2m4p-A4c5d6UWIoaY-90k4x51xCo_e/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/navneetoo7/",
   insta:
     "https://instagram.com/nef__oo7?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D",
